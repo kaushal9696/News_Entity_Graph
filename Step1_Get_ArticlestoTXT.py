a884f835a99b0194835714a4b6181c268d8334b2
Kaushal Prajapati <76026791+kaushal9696@users.noreply.github.com>
2020-12-17 16:14:37 -0500

    Update Step1_Get_ArticlestoTXT.py

--- Step1_Get_ArticlestoTXT.py
+++ Step1_Get_ArticlestoTXT.py
@@ -4,9 +4,8 @@ from newsapi import NewsApiClient
 News_API = NewsApiClient(api_key='YOUR_API_KEY')
 all_articles = News_API.get_everything(q='Covid',
                                       language='en',
-                                      page=1,
                                       sort_by='relevancy',
-                                      page_size=100,
+                                      page_size=10000,
                                       )
 
 with open('data.txt', 'w') as NewFile1:
